c6860b3a793c6fe2db8cbbaa0e9517b44a239916
Vishalgit5001 <97425204+Vishalgit5001@users.noreply.github.com>
2022-01-11 15:52:03 +0530

    changes for v2

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App(){
       <h1>GET API CALL</h1>
       <table border="5" >
         <tr>
-          <td>id</td>
-          <td>name</td>
-          <td>year</td>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Year</td>
           <td>Color</td>
-          <td>pantone_value</td>
+          <td>Pantone_Value</td>
         </tr>
        {
          data.map((item)=>
