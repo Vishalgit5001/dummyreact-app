c8c3663a0ffe7ec6508d544c2b7346395a3264d6
Vishalgit5001 <97425204+Vishalgit5001@users.noreply.github.com>
2022-01-11 16:06:51 +0530

    changes for v2

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ function App(){
       })
     })
   },[])
- console.warn("ApiData",data)
+ console.warn("Api Data",data)
   return(
     <div className="App">
-      <h1>GET API CALL</h1>
+      <h1> GET API CALL </h1>
       <table border="5" >
         <tr>
-          <td>ID</td>
-          <td>Name</td>
-          <td>Year</td>
-          <td>Color</td>
-          <td>Pantone_Value</td>
+          <td>Id</td>
+          <td>name</td>
+          <td>year</td>
+          <td>color</td>
+          <td>pantone_Value</td>
         </tr>
        {
          data.map((item)=>
